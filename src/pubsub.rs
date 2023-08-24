83576070d530b4fd04e21ecd1af6ffd4b150509a
CrazyFluff <=>
2023-08-23 19:55:07 -0700

    refactor: move pubsub related code

--- src/pubsub.rs
+++ src/pubsub.rs
@@ -1,10 +1,19 @@
+use axum::{
+    body::Body,
+    extract::{Path, Query},
+    http::Request,
+    http::StatusCode,
+    routing::{get, post},
+    Extension, Router,
+};
 use chrono::DateTime;
+use futures::{lock::Mutex, Future};
 use quick_xml::de::from_str;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
-use std::collections::HashMap;
+use std::{collections::HashMap, pin::Pin, sync::Arc};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct YoutubeLink {
+pub struct Link {
     #[serde(rename = "@rel")]
     pub rel: String,
     #[serde(rename = "@href")]
@@ -12,21 +21,21 @@ pub struct YoutubeLink {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct YoutubeAuthor {
+pub struct Author {
     pub name: String,
     pub uri: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct YoutubeEntry {
+pub struct Entry {
     pub id: String,
     #[serde(rename = "videoId")]
     pub video_id: String,
     #[serde(rename = "channelId")]
     pub channel_id: String,
     pub title: String,
-    pub link: YoutubeLink,
-    pub author: YoutubeAuthor,
+    pub link: Link,
+    pub author: Author,
     #[serde(deserialize_with = "de_time", serialize_with = "se_time", default)]
     pub published: Option<DateTime<chrono::Utc>>,
     #[serde(deserialize_with = "de_time", serialize_with = "se_time", default)]
@@ -34,14 +43,14 @@ pub struct YoutubeEntry {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct YoutubeFeed {
+pub struct Feed {
     pub title: String,
     #[serde(default)]
-    pub link: Vec<YoutubeLink>,
+    pub link: Vec<Link>,
     #[serde(deserialize_with = "de_time", serialize_with = "se_time", default)]
     pub updated: Option<DateTime<chrono::Utc>>,
     #[serde(default)]
-    pub entry: Vec<YoutubeEntry>,
+    pub entry: Vec<Entry>,
 }
 
 /// Deserialize a `DateTime` from an RFC 3339 date string.
@@ -77,33 +86,64 @@ where
     }
 }
 
-#[derive(Debug)]
+pub type CallbackHandler =
+    Box<dyn FnOnce(String) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send>;
+
+// #[derive(Debug)]
 pub struct Subscription {
-    hub: String,
     topic: reqwest::Url,
+    handler: CallbackHandler,
 }
 
-#[derive(Debug)]
+type SubscriptionID = u64;
+
+// #[derive(Debug)]
 pub struct PubSub {
     client: reqwest::Client,
-    subscriptions: HashMap<reqwest::Url, Subscription>,
-    callback_url: reqwest::Url,
+    subscriptions: Arc<Mutex<HashMap<SubscriptionID, Subscription>>>,
+    callback_path: String,
+    subscription_id: SubscriptionID,
 }
 
 impl PubSub {
-    pub fn new(callback_url: reqwest::Url) -> Self {
+    pub fn new(callback_path: &str) -> Self {
+        let subscriptions = Arc::new(Mutex::new(HashMap::new()));
+        // tracing::debug!("listening on {}", addr);
         Self {
             client: reqwest::Client::new(),
-            subscriptions: HashMap::new(),
-            callback_url,
+            subscriptions,
+            callback_path: callback_path.to_string(),
+            subscription_id: 0,
         }
     }
 
-    pub async fn subscribe(&mut self, url: reqwest::Url) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn router(&self) -> Router {
+        let router = Router::new()
+            .route("/", get(default_handler))
+            .route(
+                format!("{}/:subscription_id", self.callback_path).as_str(),
+                get(pubsub_challenge_handler),
+            )
+            .route(
+                format!("{}/:subscription_id", self.callback_path).as_str(),
+                post(pubsub_callback_handler),
+            )
+            .layer(Extension(Arc::clone(&self.subscriptions)));
+
+        router
+    }
+
+    pub async fn subscribe(
+        &mut self,
+        url: reqwest::Url,
+        handler: CallbackHandler,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let subscription_id = self.get_next_subscription_id();
         let hub = self.discover_hub(url.as_str()).await?;
 
         let mut form_data = HashMap::new();
-        form_data.insert("hub.callback", self.callback_url.as_str());
+        let pubsub_callback_url = self.get_pubsub_callback_url(subscription_id);
+        form_data.insert("hub.callback", pubsub_callback_url.as_str());
         form_data.insert("hub.topic", url.as_str());
         form_data.insert("hub.mode", "subscribe");
 
@@ -119,13 +159,11 @@ impl PubSub {
             return Err(format!("Status code not 202. {:?}", response).into());
         }
 
-        let url_clone = url.clone();
-
-        self.subscriptions.insert(
-            url,
+        self.subscriptions.lock().await.insert(
+            subscription_id,
             Subscription {
-                hub,
-                topic: url_clone,
+                topic: url,
+                handler,
             },
         );
 
@@ -138,9 +176,17 @@ impl PubSub {
         url: reqwest::Url,
     ) -> Result<(), Box<dyn std::error::Error>> {
         let hub = self.discover_hub(url.as_str()).await?;
+        let subscription_id = self.get_subscription_id(&url).await;
+
+        if subscription_id.is_none() {
+            return Err("Subscription not found".into());
+        }
+
+        let subscription_id = subscription_id.unwrap();
 
         let mut form_data = HashMap::new();
-        form_data.insert("hub.callback", self.callback_url.as_str());
+        let pubsub_callback_url = self.get_pubsub_callback_url(subscription_id);
+        form_data.insert("hub.callback", pubsub_callback_url.as_str());
         form_data.insert("hub.topic", url.as_str());
         form_data.insert("hub.mode", "unsubscribe");
 
@@ -156,7 +202,9 @@ impl PubSub {
             return Err(format!("Status code not 202. {:?}", response).into());
         }
 
-        self.subscriptions.remove(&url);
+        if let Some(subscription_id) = self.get_subscription_id(&url).await {
+            self.subscriptions.lock().await.remove(&subscription_id);
+        }
 
         println!("Unsubscribe {:?}", response);
         Ok(())
@@ -166,7 +214,7 @@ impl PubSub {
         let url = reqwest::Url::parse(url)?;
         let data = reqwest::get(url).await?.text().await?;
 
-        let feed: YoutubeFeed = from_str(&data)?;
+        let feed: Feed = from_str(&data)?;
 
         println!("Discovered Feed {:?}", feed);
 
@@ -178,4 +226,76 @@ impl PubSub {
 
         Err("No hub found".into())
     }
+
+    fn get_next_subscription_id(&mut self) -> SubscriptionID {
+        self.subscription_id += 1;
+        self.subscription_id
+    }
+
+    async fn get_subscription_id(&self, url: &reqwest::Url) -> Option<SubscriptionID> {
+        for (id, subscription) in self.subscriptions.lock().await.iter() {
+            if subscription.topic == *url {
+                return Some(*id);
+            }
+        }
+
+        None
+    }
+
+    fn get_pubsub_callback_url(&self, subscription_id: SubscriptionID) -> String {
+        let pubsub_host = std::env::var("PUBSUB_HOST").ok().unwrap();
+        let pubsub_callback_url =
+            format!("{}{}/{}", pubsub_host, self.callback_path, subscription_id);
+
+        pubsub_callback_url
+    }
+}
+
+async fn default_handler(request: Request<Body>) -> StatusCode {
+    println!("Default handler called {:?}", request);
+
+    StatusCode::OK
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PubSubCallbackParams {
+    #[serde(rename = "hub.challenge")]
+    challenge: Option<String>,
+    #[serde(rename = "hub.mode")]
+    mode: Option<String>,
+    #[serde(rename = "hub.topic")]
+    topic: Option<String>,
+    #[serde(rename = "hub.lease_seconds")]
+    lease_seconds: Option<i64>,
+}
+
+async fn pubsub_challenge_handler(
+    Query(params): Query<PubSubCallbackParams>,
+) -> (StatusCode, String) {
+    println!(
+        "Received [{:?}] challenge for topic {:?}. Subscription lasts for [{:?}] seconds. Responding with challenge {:?}.",
+        params.mode, params.topic, params.lease_seconds, params.challenge
+    );
+
+    (StatusCode::OK, params.challenge.unwrap_or_default())
+}
+
+async fn pubsub_callback_handler(
+    Extension(subscriptions): Extension<Arc<Mutex<HashMap<SubscriptionID, Subscription>>>>,
+    Path(subscription_id): Path<SubscriptionID>,
+    payload: String,
+) -> StatusCode {
+    println!("Processing feed: {}", payload);
+
+    {
+        let handler = subscriptions
+            .lock()
+            .await
+            .remove(&subscription_id)
+            .unwrap()
+            .handler;
+        handler(payload).await;
+    }
+
+    StatusCode::OK
 }
